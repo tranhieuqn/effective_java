439d50db67a5b343bc1feddeb839eea4fd3b1ad6
Hieu Tran <hieu.tran@bstarsolutions.com>
2020-03-01 23:21:56 +0700

    item2-builder pattern

--- src/com/hieutran/items/item2/Main.java
+++ src/com/hieutran/items/item2/Main.java
@@ -1,7 +1,17 @@
 package com.hieutran.items.item2;
 
+import static com.hieutran.items.item2.NyPizza.Size.SMALL;
+import static com.hieutran.items.item2.Pizza.Topping.MUSHROOM;
+import static com.hieutran.items.item2.Pizza.Topping.ONION;
+
 public class Main {
     public static void main(String[] args) {
+        NyPizza nyPizza = new NyPizza.Builder(SMALL)
+                .addTopping(MUSHROOM)
+                .addTopping(ONION)
+//                .addTopping(MUSHROOM)
 
+                .build();
+        System.out.println(nyPizza.toppings);
     }
 }
